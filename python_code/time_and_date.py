df8cc762019c4a7b9a29c3a129adb0581d5e753c
Mesfi Haftu <mesfinhaftu@gmail.com>
2024-05-04 08:19:38 +0300

    Redundant comments edited

--- python_code/time_and_date.py
+++ python_code/time_and_date.py
@@ -1,14 +1,4 @@
 #  Date & Time 14/04/2024, 21:56.
-#  @author Mesfin Haftu
-
-#  Date & Time 14/04/2024, 21:55.
-#  @author Mesfin Haftu
-
-#  Date & Time 14/04/2024, 21:54.
-#  @author Mesfin Haftu
-
-#  Date & Time (\d+)", 1, "-")14/04/2024, 21:53.
-#  @author Mesfin Haftu
 
 from datetime import date
 from datetime import timedelta
