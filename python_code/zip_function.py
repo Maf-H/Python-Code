773bc1725bb37dea4374bea839d92ec283cc063b
Maf-H <67267690+Maf-H@users.noreply.github.com>
2024-06-13 23:23:16 +0300

    Update zip_function.py

--- python_code/zip_function.py
+++ python_code/zip_function.py
@@ -1,15 +1,6 @@
 #  Date & Time 14/04/2024, 21:56.
 #  @author Mesfin Haftu
 
-#  Date & Time 14/04/2024, 21:55.
-#  @author Mesfin Haftu
-
-#  Date & Time 14/04/2024, 21:54.
-#  @author Mesfin Haftu
-
-#  Date & Time (\d+)", 1, "-")14/04/2024, 21:53.
-#  @author Mesfin Haftu
-
 username = ['Mesfin', 'John', 'Doe']
 passwords = ('P@ssword', 'abc123', 'guest')
 
