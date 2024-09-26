6286374a375849319efd7d67296b21fd5a389042
Mesfin Haftu <mhtr03@gmail.com>
2024-09-26 20:37:28 +0300

    Add .gitignore file to tell which file types to ignore

--- python_code/request_demo.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-r = requests.get("http://localhost:3000/")
-if r.status_code == requests.codes.ok:
-    print("Successful connection")
-    print(r.headers['Content-Type'])
-
--- python_code/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-int(0)))
-print(post_order_traverse(int(0)))
-
